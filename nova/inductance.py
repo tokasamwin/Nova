1956f02518dc533d4fd502ad67c4a853e064a80d
Simon <twistersi@gmail.com>
2016-06-15 10:44:48 +0100

    TF inductance updates

--- nova/inductance.py
+++ nova/inductance.py
@@ -1,54 +1,74 @@
 import numpy as np
 from itertools import combinations_with_replacement as cr
+from itertools import combinations as cm
 
 mu_o = 4*np.pi*1e-7  # magnetic constant [Vs/Am]
-
-def dloop(X):
-    dX = np.diff(np.append(X,X[0,:,None].T,axis=0),axis=0)
-    return dX
-    
-def rotate(theta):
-    Rz = np.array([[np.cos(theta),-np.sin(theta),0],
-                   [np.sin(theta),-np.cos(theta),0],
-                   [0,0,1]])
-    return Rz
     
 class neumann(object):
-    def __init__(self,r,**kwargs):
-        self.r = r
+    def __init__(self,**kwargs):
+        if 'r' in kwargs:
+            self.setr(kwargs.get('r'))
         if 'X' in kwargs:
             self.setX(kwargs.get('X'))
         if 'X_' in kwargs:
             self.setX_(kwargs.get('X_'))
             
+    def dloop(self,X):
+        dX = np.diff(np.append(X,X[0,:,None].T,axis=0),axis=0)
+        return dX
+            
     def setX(self,X):
         self.X = X
-        self.dX = dloop(X)
+        self.dX = self.dloop(X)
         
     def setX_(self,X_):
         self.X_ = X_
-        self.dX_ = dloop(X_)
+        self.dX_ = self.dloop(X_)
         
     def setr(self,r):
         self.r = r
         
-    def intergrate(self,ij):
-        dr = np.linalg.norm(self.X[ij[0],:]-self.X_[ij[1],:])
-        if dr > self.r/2:  # mutual inductance
-            L = np.dot(self.dX[ij[0],:],self.dX_[ij[1],:])/dr
+    def intergrate(self,ij,X,X_,dX,dX_,r):
+        dr = np.linalg.norm(X[ij[0],:]-X_[ij[1],:])
+        if dr > r/2:  # mutual inductance
+            L = np.dot(dX[ij[0],:],dX_[ij[1],:])/dr
         else:  # self inductance
             L = 0
         if ij[0] != ij[1]:
             L *= 2  # upper + lower
         return L
         
+    def substeps(self,ij):
+        L = 0
+        dl = np.linalg.norm(self.dX[ij[0],:])
+        if dl > self.r:
+            N = int(np.ceil(15*dl/self.r))
+            dx = dl/N
+            X = np.zeros((N,3))
+            dX = np.zeros((N,3))
+            X[:,0] = np.linspace(dx/2,dl-dx/2,N)
+            dX[:,0] = dx
+            selfloop = cm(range(N),2)
+            for kl in selfloop:
+                L += self.intergrate(kl,X,X,dX,dX,self.r)
+        return L
+        
     def calculate(self):
         loop = cr(range(len(self.X)),2)
         L = 0
-        for ij in loop:
-            L += self.intergrate(ij)
         if np.linalg.norm(self.X-self.X_) == 0:  # self inductance correction
-            L += np.sum(np.linalg.norm(self.dX,axis=1))/2
+            L += (np.sum(np.linalg.norm(self.dX,axis=1)))/2
+            mutual = False
+        else:
+            mutual = True
+        for ij in loop:
+            if ij[0] != ij[1] or mutual:
+                L += self.intergrate(ij,self.X,self.X_,self.dX,self.dX_,self.r)
+            else:
+                dl = np.linalg.norm(self.dX[ij[0],:])
+                if dl > self.r:
+                    L += 2*np.log(dl/self.r)
+                #L += self.substeps(ij)  # self inductance sub-calc
         L *= mu_o/(4*np.pi) 
         return L
     
--- nova/tf_test.py
+++ nova/tf_test.py
@@ -3,12 +3,14 @@ from nova.config import Setup
 from nova.streamfunction import SF
 from nova.radial_build import RB
 from nova.coils import PF,TF
-from etna.coil_geom import configure
+from etna.coil_geom import configure,coil_cage
 from etna.coil_apdl import coil_map
-from inductance import rotate,neumann,dloop
+from inductance import neumann
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
-
+from time import time
+import sys
+import datetime
 
 import seaborn as sns
 rc = {'figure.figsize':[10,10*12/16],'savefig.dpi':100, # 
@@ -19,63 +21,79 @@ sns.set(context='talk',style='white',font='sans-serif',palette='Set2',
 
 cgeom = configure('TF',Ndp=0,Nloop=0)
 cmap = coil_map(cgeom)
-cmap.wrap()
-cgeom.segment(np.mean(cmap.loops['x']),50)
+cgeom.segment(cmap.loops,100)
 
-
-'''  
 nTF = 18
 theta = np.linspace(0,2*np.pi,nTF,endpoint=False)
-#theta = theta[:int(np.floor(nTF/2+1))]
-N = len(theta)  # cmap.Ncond+
-
-def load_coil(index):
-    if 1==1:  # index >= cmap.Ncond:  # rotated TF coils
-        k = index#-cmap.Ncond
-        X = cgeom.loop(np.mean(cmap.loops['x']),0)
-        X = np.dot(X,rotate(theta[k]))
-        r = cgeom.pn*np.sqrt(134/np.pi)
-        nturns = 134
-    else:
-        X = cgeom.loop(cmap.loops['x'][index],cmap.loops['y'][index])
-        r = np.sqrt(235.3e-6/np.pi) 
-        nturns = 1
-    return X,nturns,r
+ 
+r_coil = cgeom.pn*np.sqrt(134/np.pi)
+r_strand = cgeom.r_cond
+#r_strand = np.sqrt(235.3e-6/np.pi)
+cage = coil_cage(cgeom.profile,cmap.loops,theta,r_coil,r_strand)
+neu = neumann()
     
 fig = pl.figure()
 ax = fig.gca(projection='3d')
-
-neu = neumann(np.sqrt(235.3e-6/np.pi))
+N = cmap.Ncond+len(theta)  
+N = 20
 M = np.zeros((N,N))
+to = time()
 for i in np.arange(0,N):
-    print(i)
-    X,nturns,r = load_coil(i)
+    X,nturns,r = cage.load(i)
     neu.setX(X)
     neu.setr(r)
-    ax.plot(np.append(X[:,0],X[0,0]),
-            np.append(X[:,1],X[0,1]),
+    ax.plot(np.append(X[:,0],X[0,0]),np.append(X[:,1],X[0,1]),
             np.append(X[:,2],X[0,2]))
     for j in np.arange(i,N):
-        X_,nturns_ = load_coil(j)[:2]
+        X_,nturns_ = cage.load(j)[:2]
         neu.setX_(X_)
         M[i,j] = nturns*nturns_*neu.calculate()
         if i != j:
             M[j,i] = M[i,j]  # copy to lower
+    t1 = time()
+    t_fraction = 2*((i+1)*(N-(i+1)/2))/N**2
+    t_wall = t1-to
+    t_total = t_wall/t_fraction
+    t_remain = t_total-t_wall
+    t_str = '\rtotal: '+str(datetime.timedelta(seconds=np.round(t_total)))+'\t'
+    t_str += 'complete:{:3.0f}%\t'.format(100*t_fraction)
+    t_str += 'remaining: '+str(datetime.timedelta(seconds=np.round(t_remain)))
+    sys.stdout.write(t_str)
+    sys.stdout.flush()
+print('')
+
+
 
 ax.auto_scale_xyz([-10,10],[-10,10],[-15,5])   
 np.set_printoptions(threshold=100,edgeitems=5,precision=3,linewidth=150)
-
 print(M)
 print(np.sum(M))
-'''
 
-X = cgeom.loop(np.mean(cmap.loops['x']),0)
-X_ = np.dot(X,rotate(theta[0]))
-r = cgeom.pn*np.sqrt(134/np.pi)
-neu = neumann(r)
-neu.setX(X)
-neu.setX_(X_)
-print(134**2*neu.calculate())
+
+'''
+N = np.linspace(10,150,20)
+L = np.zeros(len(N))
+dX = np.zeros(len(N))
+for i,Nseg in enumerate(N):
+    print(i)
+    cgeom.segment(np.mean(cmap.loops['x']),Nseg)
+    X = cgeom.loop(np.mean(cmap.loops['x']),0)
+    X_ = X
+    r = cgeom.pn*np.sqrt(134/np.pi)
+    neu = neumann(r)
+    neu.setX(X)
+    neu.setX_(X_)
+    L[i] = 134**2*neu.calculate()
+    dX[i] = np.mean(np.linalg.norm(neu.dX,axis=1))
+#print(np.mean(np.linalg.norm(neu.dX,axis=1)),neu.r)
+#print()
+    
+fig,ax = pl.subplots(2,1)
+ax[0].plot(N,L)
+ax[0].plot(N,0.349*np.ones(len(N)))
+ax[1].plot(N,dX)
+ax[1].plot(N,neu.r*np.ones(len(N)))
+'''
 
 '''
 mu_o = 4*np.pi*1e-7  # magnetic constant [Vs/Am]
@@ -99,7 +117,6 @@ Xo[:,2] = z[:-1]+np.diff(z)/2
 fig = pl.figure()
 ax = fig.gca(projection='3d')
 '''
-
 
 '''
 X = cgeom.loop(np.mean(cmap.loops['x']),0) 
@@ -125,7 +142,6 @@ for t in theta:
     
     #print('{:1.5f}'.format(134**2*induce(X,X_,cgeom.pn*np.sqrt(134)/np.pi)))
     ax.plot(X_[:,0],X_[:,1],X_[:,2])
-    
 
 ax.auto_scale_xyz([-10,10],[-10,10],[-15,5])   
 '''
